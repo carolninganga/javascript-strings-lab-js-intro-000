050e872ff6731279f2991d89e3eb2021de8f4b6a
Caroline Ninganga <carolninganga@gmail.com>
2019-04-04 12:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Hello, ${specialGuest}! High ${3 + 2}!`;
+var conversation = "Let's talk about" + topic;
